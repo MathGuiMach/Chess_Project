751a1e849f705413c86f508ca3c07431ce18bce4
MathGuiMach <mathguimach@gmail.com>
2020-10-15 11:00:30 -0300

    UI improvement

--- src/application/Application.java
+++ src/application/Application.java
@@ -4,6 +4,11 @@ import chess.ChessMatch;
 
 public class Application {
 
+	//HOW TO RUN 
+	//GIT BASH
+	//command1: cd /c/WorkSpaces/ws_Eclipse/chess_project/bin
+	//command2: java application/Application
+	
 	public static void main(String[] args) {
 		ChessMatch chessMatch = new ChessMatch();
 		UI.printBoard(chessMatch.getPieces());
